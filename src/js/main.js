50af347bc015a0b2106030074229f60f51a51640
Trevorcofc <reddingtd@g.cofc.edu>
2025-06-27 11:19:40 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 // login logic
-function login(event) {
-  event.preventDefault();
-
+function login(role) {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   if (username && password) {
-    window.location.href = "../student_dash/index.html";
+    if (role === "student") {
+      window.location.href = "../student_dash/index.html";
+    } else if (role === "teacher") {
+      window.location.href = "../teacher_dash/index.html";
+    }
   } else {
     alert("Please enter both username and password.");
   }
 }
+
