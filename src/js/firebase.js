78c2b14c65d8cd9459f03c39a1175382e23f964e
Trevorcofc <reddingtd@g.cofc.edu>
2025-08-16 18:31:03 -0400

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,1 +1,11 @@
+<!-- src/js/firebase.js -->
+<script type="module">
+  // Replace the config below with the one from Firebase console
+  export const firebaseConfig = {
+    apiKey: "YOUR_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    appId: "YOUR_APP_ID",
+  };
+</script>
 
