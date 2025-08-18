51d4f142a487d26c2612657e402016f5d8794e20
Trevorcofc <reddingtd@g.cofc.edu>
2025-08-18 18:10:14 -0400

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,11 +1,11 @@
-<!-- src/js/firebase.js -->
-<script type="module">
+//src/js/firebase.js
   
   export const firebaseConfig = {
     apiKey: "AIzaSyAN5bczyFGapDT9vcqOAxI473SLUDfSHxA",
     authDomain: "ethicadmey.firebaseapp.com",
     projectId: "ethicadmey",
+    storageBucket: "ethicadmey.firebasestorage.app",
+    messagingSenderId: "281835928268",
     appId: "1:281835928268:web:49d817295d84b409c6e069",
   };
-</script>
 
